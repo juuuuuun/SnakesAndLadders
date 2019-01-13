0d7628d137fefa40637d8cf9d0d6325414d6286f
Jun Oh <kyungjunjoh@gmail.com>
2019-01-12 18:49:09 -0800

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Jun Oh on 2019-01-12.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
